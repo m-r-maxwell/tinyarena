f55ccfd299b09b394e06a1d28b06d13b1f0a9fae
Matt Maxwell <mrmaxwell78@gmail.com>
2023-05-17 21:31:23 -0400

    Damage text

--- scenes/component/hurt_box_component.gd
+++ scenes/component/hurt_box_component.gd
@@ -3,6 +3,10 @@ class_name HurtboxComponent
 
 @export var health_component: HealthComponent
 
+#this is a way to add a scene without adding it to the inspector, but
+#this follows the hardcoded = bad ideaology because if this ever changes
+#the string wont update
+var floating_text_scene = preload("res://scenes/ui/floating_text.tscn")
 
 func _ready():
 	area_entered.connect(on_area_entered)
@@ -17,3 +21,9 @@ func on_area_entered(other_area: Area2D):
 	
 	var hitbox_compoent = other_area as HitBoxComponent
 	health_component.damage(hitbox_compoent.damage)
+	
+	var floating_text = floating_text_scene.instantiate() as Node2D
+	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
+	
+	floating_text.global_position = global_position + (Vector2.UP * 16)
+	floating_text.start(str(hitbox_compoent.damage))
