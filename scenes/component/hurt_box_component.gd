09b7692047b10f0b38e59d8a8c883061b108a214
Matt Maxwell <mrmaxwell78@gmail.com>
2023-05-21 20:57:57 -0400

    ui stuff

--- scenes/component/hurt_box_component.gd
+++ scenes/component/hurt_box_component.gd
@@ -26,4 +26,8 @@ func on_area_entered(other_area: Area2D):
 	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
 	
 	floating_text.global_position = global_position + (Vector2.UP * 16)
-	floating_text.start(str(hitbox_compoent.damage))
+	
+	var format_string = "%0.1f"
+	if round(hitbox_compoent.damage) == hitbox_compoent.damage:
+		format_string = "%0.0f"
+	floating_text.start(format_string %hitbox_compoent.damage)
