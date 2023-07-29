cf8996706785f16c95b72d0c33e80a7642d5a4ce
Matt Maxwell <mrmaxwell78@gmail.com>
2023-07-29 17:16:27 -0400

    Animation stuff

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -5,8 +5,34 @@ extends PanelContainer
 
 signal selected
 
+var disabled = false
+
 func _ready():
 	gui_input.connect(on_gui_input)
+	mouse_entered.connect(on_mouse_entered)
+
+
+func play_in(delay: float = 0):
+	modulate = Color.TRANSPARENT
+	scale = Vector2.ZERO 
+	await get_tree().create_timer(delay).timeout
+	$AnimationPlayer.play("in")
+
+
+func play_discard():
+	$SelectedAnimationPlayer.play("discard")
+	
+func select_card():
+	disabled = true
+	$SelectedAnimationPlayer.play("selected")
+	for other_card in get_tree().get_nodes_in_group("upgrade_card"):
+		if other_card == self:
+			continue
+		other_card.play_discard()
+	
+	await $SelectedAnimationPlayer.animation_finished
+	selected.emit()
+		
 
 func set_ability_upgrade(upgrade: AbilityUpgrade):
 	name_label.text = upgrade.name
@@ -14,6 +40,13 @@ func set_ability_upgrade(upgrade: AbilityUpgrade):
 
 
 func on_gui_input(event: InputEvent):
+	if disabled:
+		return
 	#input mapping in settings to check based on name instead of key click
 	if event.is_action_pressed("left_click"):
-		selected.emit()
+		select_card()
+
+func on_mouse_entered():
+	if disabled:
+		return
+	$HoverAnimationPlayer.play("hover")
