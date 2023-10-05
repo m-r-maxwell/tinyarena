a06d397d81806945d5253cc5d890206316f9b2e9
Matt Maxwell <mrmaxwell78@gmail.com>
2023-10-04 21:21:09 -0400

    adding more sounds

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -74,6 +74,7 @@ func on_damage_timer_timeout():
 func on_health_changed():
 	GameEvents.emit_player_damager()
 	update_health_display()
+	$HitRandomStreamPlayer.play_random()
 
 
 func on_ability_upgrade_added(ability_upgrade: AbilityUpgrade, current_upgrades: Dictionary):
