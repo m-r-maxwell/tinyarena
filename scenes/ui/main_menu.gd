d28c7d995299e06f50225522dd682007d8dd72c5
Matt Maxwell <mrmaxwell78@gmail.com>
2023-10-09 21:05:06 -0400

    Opshun menu

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -1,5 +1,6 @@
 extends CanvasLayer
 
+var options = preload("res://scenes/ui/options_menu.tscn")
 
 func _ready():
 	$%PlayButton.pressed.connect(on_play_pressed)
@@ -12,8 +13,14 @@ func on_play_pressed():
 	
 
 func on_options_pressed():
-	pass
+	var options_instace = options.instantiate()
+	add_child(options_instace)
+	options_instace.back_pressed.connect(on_options_closed.bind(options_instace))
 	
 
 func on_quit_pressed():
 	get_tree().quit()
+
+
+func on_options_closed(options_instance: Node):
+	options_instance.queue_free()
